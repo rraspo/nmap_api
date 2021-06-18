6eb28841ef4bbdd86d19feff6790a4f5b8c7e8eb
Juan A. Porras <juanabrahamporras@gmail.com>
2021-06-18 13:45:04 -0500

    Add middlewares for file upload. Rename api routes.

--- app.js
+++ app.js
@@ -1,25 +1,19 @@
-const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const db = require('./database');
+const createError = require('http-errors');
+const morgan = require('morgan');
+const fileUpload = require('express-fileupload');
+const apiRoutes = require('./server/scans');
 
 const app = express();
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+app.use(fileUpload({
+  createParentPath: true
+}));
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', apiRoutes);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
