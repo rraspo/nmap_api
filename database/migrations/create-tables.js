6117a9d2f9dc8a3611ebbbdd42519366b43edac9
Juan A. Porras <juanabrahamporras@gmail.com>
2021-06-18 13:50:52 -0500

    Fix lint errors. Add close statement for seeding and migrating tables.

--- database/migrations/create-tables.js
+++ database/migrations/create-tables.js
@@ -1,15 +1,16 @@
-const db = require ('../../database');
+const db = require('..');
+
 const sql_create = `CREATE TABLE IF NOT EXISTS Hosts (
   Host_ID INTEGER PRIMARY KEY AUTOINCREMENT,
   IP VARCHAR(100) NOT NULL,
   HOSTNAME VARCHAR(100)
 );`;
 
-console.log(db);
-
+/* eslint-disable no-console, consistent-return */
 module.exports = db.run(sql_create, (err) => {
   if (err) {
-    return console.error(err.message);
+    console.error(err.message);
   }
   console.log('Successful creation of the \'Hosts\' table');
-});
+}).close();
+/* eslint-enable no-console, consistent-return */
--- database/seeds/seed-tables.js
+++ database/seeds/seed-tables.js
@@ -1,14 +1,15 @@
-const db = require ('../../database');
+const db = require('..');
+
 const sql_insert = `INSERT INTO Hosts (Host_ID, IP, HOSTNAME) VALUES
   (1, '81.107.115.203', 'cpc123026-glen5-2-0-cust970.2-1.cable.virginm.net'),
   (2, '158.69.205.102', 'nicholas.cybershark.net'),
   (3, '193.22.92.195', 'loghermes.sysraildata.com');`;
 
-console.log(db);
-
+/* eslint-disable no-console, consistent-return */
 module.exports = db.run(sql_insert, (err) => {
   if (err) {
-    return console.error(err.message);
+    console.error(err.message);
   }
   console.log('Successful seed of the \'Hosts\' table');
 }).close();
+/* eslint-disable no-console, consistent-return */
