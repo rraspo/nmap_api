f10a63befa16d0931a6c78ab0751df2ab8e2ad5f
Juan A. Porras <juanabrahamporras@gmail.com>
2021-06-21 10:43:08 -0500

    Add Scans table migration. Fix seeds.

--- database/migrations/create-tables.js
+++ database/migrations/create-tables.js
@@ -1,16 +1,37 @@
 const db = require('..');
 
-const sql_create = `CREATE TABLE IF NOT EXISTS Hosts (
-  Host_ID INTEGER PRIMARY KEY AUTOINCREMENT,
-  IP VARCHAR(100) NOT NULL,
-  HOSTNAME VARCHAR(100)
-);`;
+const create_hosts_table = `
+  CREATE TABLE IF NOT EXISTS Hosts (
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    IP VARCHAR(100) NOT NULL UNIQUE
+  );`;
+
+const create_scans_table = `
+  CREATE TABLE IF NOT EXISTS Scans (
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    PORT INTEGER NOT NULL,
+    STATUS VARCHAR(100) NOT NULL,
+    TRANSPORT VARCHAR(100) NOT NULL,
+    PROTOCOL VARCHAR(100) NOT NULL,
+    SCAN_DATE VARCHAR(22) NOT NULL,
+    Host_ID INTEGER NOT NULL,
+    FOREIGN KEY (Host_ID) REFERENCES Hosts (ID),
+    UNIQUE(Host_ID, PORT, STATUS, SCAN_DATE)
+  );`;
 
 /* eslint-disable no-console, consistent-return */
-module.exports = db.run(sql_create, (err) => {
-  if (err) {
-    console.error(err.message);
-  }
-  console.log('Successful creation of the \'Hosts\' table');
-}).close();
+module.exports = (() => {
+  db.run(create_hosts_table, (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Successful creation of the \'Hosts\' table.');
+  });
+  db.run(create_scans_table, (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Successful creation of the \'Scans\' table.');
+  }).close();
+})();
 /* eslint-enable no-console, consistent-return */
--- database/seeds/seed-tables.js
+++ database/seeds/seed-tables.js
@@ -1,9 +1,9 @@
 const db = require('..');
 
-const sql_insert = `INSERT INTO Hosts (Host_ID, IP, HOSTNAME) VALUES
-  (1, '81.107.115.203', 'cpc123026-glen5-2-0-cust970.2-1.cable.virginm.net'),
-  (2, '158.69.205.102', 'nicholas.cybershark.net'),
-  (3, '193.22.92.195', 'loghermes.sysraildata.com');`;
+const sql_insert = `INSERT INTO Hosts (Host_ID, IP) VALUES
+  (1, '81.107.115.203'),
+  (2, '158.69.205.102'),
+  (3, '193.22.92.195');`;
 
 /* eslint-disable no-console, consistent-return */
 module.exports = db.run(sql_insert, (err) => {
